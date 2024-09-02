fcb31156a2c9cc580aafec16289914d0b22e269d
bhargav yarlagadda <119490919+bhargav-yarlagadda@users.noreply.github.com>
2024-09-02 15:54:40 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Gear from './test/Gear'
+const App = () => {
+  return (
+    <div>
+      <Gear/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/test/Gear.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Gear.css'
+const Gear = () => {
+  return (
+    <div id="wrapper">
+    <div className="gears" id="two-gears">
+      <div className="gears-container">
+        <div className="gear-rotate">
+  
+        </div>
+        <div className="gear-rotate-left">
+
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Gear
